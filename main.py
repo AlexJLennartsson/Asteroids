537e236ba36de8b2c8d6f9ceb1163f402e74bf30
AlexJLennartsson <alexlennartsson@gmail.com>
2024-11-15 15:49:40 +0000

    added gitignore and set it up properly

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from constants import *
 
 def main():
   print("Starting asteroids!\nScreen width: 1280\nScreen height: 720")
+  pygame.init()
+  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+  while True:
+    screen.fill((0,0,0))
+    pygame.display.flip()
 
 if __name__ == "__main__":
   main()
