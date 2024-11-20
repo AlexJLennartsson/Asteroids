5bbbb3496f39b9d9bfd1064dcf4febc28ee59b9f
AlexJLennartsson <alexlennartsson@gmail.com>
2024-11-20 18:38:04 +0000

    added clock object as well as circleshape module

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ def main():
   print("Starting asteroids!\nScreen width: 1280\nScreen height: 720")
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  clock = pygame.time.Clock()
+  dt = 0
 
   while True:
     for event in pygame.event.get():
@@ -12,6 +14,7 @@ def main():
         return
     screen.fill((0,0,0))
     pygame.display.flip()
+    dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
   main()
